8d0ed42574a4e4c03e0e18b978b8b5c471d292a4
mjs249 <134558869+mjs249@users.noreply.github.com>
2024-02-19 15:18:14 -0500

    Create whoisthere.sh

---
+++ whoisthere.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/sh
+
+echo "Users:\n"
+while IFS= read -r line
+do
+    val=$(echo $line | cut -d ":" -f7)
+    user=$(echo $line | cut -d ":" -f1)
+    if [ "$val" != "/bin/false" -a "$val" != "/usr/sbin/nologin" ]; then 
+        echo "$user has $val"
+    fi
+done < "/etc/passwd"
+
+echo "\n\nPasswords:\n"
+while IFS= read -r line
+do
+    val=$(echo $line | cut -d ":" -f2)
+    user=$(echo $line | cut -d ":" -f1)
+    if [ ${#val} != "1" ]; then 
+        echo "$user has a password"
+    fi
+done < "/etc/shadow"
+
+echo "\nSudo Permissions:\n"
+grep -E '^[%a-zA-Z]' /etc/sudoers | grep -v '^#' | while IFS= read -r line
+do
+    if echo "$line" | grep -q '^%'; then
+        echo "Group $(echo $line | cut -d ' ' -f1) has sudo access"
+    else
+        echo "User $(echo $line | cut -d ' ' -f1) has sudo access"
+    fi
+done
+
+echo "\nUsers in the sudo group:\n"
+if getent group sudo > /dev/null; then
+    sudo_members=$(getent group sudo | cut -d ':' -f4)
+    if [ -n "$sudo_members" ]; then
+        echo "$sudo_members" | tr ',' '\n' | while IFS= read -r member; do
+            echo "- $member"
+        done
+    else
+        echo "No members found in the sudo group."
+    fi
+else
+    echo "The sudo group does not exist on this system."
+fi
